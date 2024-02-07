c3ad1806776e0eacc389e73eec427ae658a8c7ef
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2024-02-07 09:17:25 -0500

    check cov of K and U

--- src/MA.jl
+++ src/MA.jl
@@ -13,6 +13,7 @@ abstract type ModalAnalysisAlgorithm end
 #TODO: Move to conf file
 const global TEMPERATURE_COL::Int64 = 2
 const global POTENTIAL_ENG_COL::Int64 = 3 #for both thermo_data and eq.engs
+const global KINETIC_ENG_COL::Int64 = 4
 const global FC_TOL::Float64 = 1e-12
 const global DUMP_NAME::String = "dump.atom"
 const global EQ_POSN_NAME::String = "equilibrium.atom"
@@ -108,9 +109,19 @@ function parse_simulation_data(path::String)
     #Load Thermo Data & Masses
     thermo_data = readdlm(thermo_path, comments = true);
     potential_eng_MD = thermo_data[:,POTENTIAL_ENG_COL]
+    kinetic_eng_MD = thermo_data[:,KINETIC_ENG_COL]
     temps_MD = thermo_data[:, TEMPERATURE_COL]
 
     T_avg = mean(temps_MD)
+
+    # Check that assumption of Var(U) + Var(K) = Var(K + U) is valid
+    tot_var = var(potential_eng_MD + kinetic_eng_MD)
+    covar = 2*cov(potential_eng_MD, kinetic_eng_MD)
+
+    if covar > 0.01*tot_var
+        @warn "Covaraicne accounts for more than 1% of heat capacity. Var(U + K) linearly assumption
+         could be invalid. Covaraince: $(covar) Total Variance: $(tot_var)"
+    end
 
     return atom_masses, potential_eng_MD, T_avg, eq, ld
 
@@ -123,7 +134,7 @@ function parse_eq_energy(path::String)
 end
 
 function check_temp(T, T_actual::Float64)
-    if !isapprox(T, T_actual, atol = 1e-1)
+    if !isapprox(T, T_actual, atol = 1)
         @warn "ModalAnalysisAlgorithm expected temperature: $(T) but MD simulation average was $(T_actual)"
     end
 end
