a8940631aa1399714ede10cca38f8131cedb71b0
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2024-02-23 14:32:20 -0500

    Update MonteCarloJobs.jl

--- src/workflows/MonteCarloJobs.jl
+++ src/workflows/MonteCarloJobs.jl
@@ -10,30 +10,48 @@ Currently no parallelization within a simulation, run as many seeds as cores ava
 creates 0.5 GB of position storage plus other storage. Ensure you have ~ 1GB of RAM per seed plus
 enough to store the second and third order force constants in RAM.
 """
-function MonteCarloMAJob(sys::SuperCellSystem{D}, TEP_path::String,
+function MonteCarloMAJob(sys::SuperCellSystem{D}, TEP_path::Function,
      n_steps::Int, n_steps_equil::Int, outpath::String,
-     temp, kB, data_interval::Int, n_seeds::Int, length_scale; output_type = :NMA,
+     temperatures::AbstractVector{<:Real}, kB, data_interval::Int, n_seeds::Int, length_scale; output_type = :NMA,
      F2_name::String = "F2", F3_name::String = "F3") where D
 
     if output_type ∉ (:NMA, :INMA)
         error("output_type must be :NMA or :INMA got $(output_type)")
     end
 
-    F2, F3 =  load(TEP_path, F2_name, F3_name)
+    if Threads.nthreads() < length(temperatures)
+        error("Cannot have more temperatures: $(length(temperatures)) than threads: $(Threads.nthreads()).")
+    end
 
-    step_size_std, percent_accepted = pick_step_size(sys, 10000, length_scale, F2, F3, temp, kB)
-    @info "Chose step size std: $(step_size_std) for temperature: $(temp)K with $(percent_accepted)% accepted."
+    cores_per_temp = floor(Threads.nthreads() / length(temperatures))
 
-    Threads.@threads for seed in 1:n_seeds
-        outpath_seed = joinpath(outpath, "seed$(seed)")
-        mkdir(outpath_seed)
-        sim = MC_Simulation(n_steps, n_steps_equil, step_size_std, temp, kB, deepcopy(positions(sys)))
+    #Pick step sizes based on temperatures
+    step_size_stds = zeros(length(temperatures))
+    Threads.@threads for (i,temp) in enumerate(temperatures)
+        step_size_stds[i], percent_accepted = pick_step_size(sys, 10000, length_scale, F2, F3, temp, kB)
+        @info "Chose step size std: $(step_size_std) for temperature: $(temp)K with $(percent_accepted)% accepted."
+    end
 
-        U_arr, num_accepted = runMC(sys, sim, outpath_seed, output_type, data_interval, F2, F3)
 
-        jldsave(joinpath(outpath_seed, "MC_stats.jld2"), 
-                U_arr = U_arr, percent_accepted = 100*num_accepted/sim.n_steps,
-                step_size_std = step_size_std)
+    Threads.@threads for (i,temp) in enumerate(temperatures)
+        F2, F3 =  load(TEP_path(temp), F2_name, F3_name)
+
+        Threads.@threads for chunk_i in 1:cores_per_temp
+            for seed in chunk_i:cores_per_temp:n_seeds
+
+                outpath_seed = joinpath(outpath, "seed$(seed)")
+                mkdir(outpath_seed)
+
+                sim = MC_Simulation(n_steps, n_steps_equil, step_size_std[i], temp, kB, deepcopy(positions(sys)))
+
+                U_arr, num_accepted = runMC(sys, sim, outpath_seed, output_type, data_interval, F2, F3)
+
+                jldsave(joinpath(outpath_seed, "MC_stats.jld2"), 
+                        U_arr = U_arr, percent_accepted = 100*num_accepted/sim.n_steps,
+                        step_size_std = step_size_std)
+            end
+            
+        end
     end
 
 end
